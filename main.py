fa83b672157e8caacb62c0c56ed0c1eefc6a76ae
wxy1343 <1343890272@qq.com>
2022-12-26 13:21:25 +0800

    update

--- main.py
+++ main.py
@@ -98,7 +98,8 @@ class ManifestAutoUpdate:
                 self.set_depot_info(depot_id, manifest_gid)
                 app_repo = git.Repo(app_path)
                 with lock:
-                    app_repo.git.rm(delete_list)
+                    if delete_list:
+                        app_repo.git.rm(delete_list)
                     app_repo.git.add(f'{depot_id}_{manifest_gid}.manifest')
                     app_repo.index.commit(f'Update depot: {depot_id}_{manifest_gid}')
                     app_repo.create_tag(f'{depot_id}_{manifest_gid}')
