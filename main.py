e8834137f5a1ca5c0d7f5522e60990b853732797
wxy1343 <1343890272@qq.com>
2023-02-17 16:15:08 +0800

    update

--- main.py
+++ main.py
@@ -450,6 +450,7 @@ class ManifestAutoUpdate:
                     if 'manifests' in depot and 'public' in depot['manifests'] and int(
                             depot_id) in {*cdn.licensed_depot_ids, *cdn.licensed_app_ids}:
                         manifest_gid = depot['manifests']['public']
+                        self.set_depot_info(depot_id, manifest_gid)
                         with lock:
                             if int(app_id) not in self.user_info[username]['app']:
                                 self.user_info[username]['app'].append(int(app_id))
